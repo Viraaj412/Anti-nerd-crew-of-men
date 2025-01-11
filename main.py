1690bf25ad21570caba07ab6f98c6f25b8163d73
Gauthamaravind <157193309+Gauthamaravind@users.noreply.github.com>
2025-01-12 00:45:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import football 
+import geograpy
+import music
 from football import start_quiz as football_quiz
 from geograpy import start_quiz as geograpy_quiz
 from music import start_quiz as music_quiz
@@ -6,17 +9,17 @@ def start_quiz():
 
     print("Welcome to the Anti-Nerd Crew of Men Quizzes!")
     print("Which quiz would you like to take?")
-    print("1. Football")
-    print("2. Geography")
-    print("3. Music")
+    print("1. football")
+    print("2. geography")
+    print("3. music")
 
     choice = input("Enter your choice (1/2/3): ")
     if choice == "1":
-        football.start_quiz()
+        football.start_quiz() 
     elif choice == "2":
-        geograpy.start_quiz()
+        geograpy.start_quiz() 
     elif choice == "3":
-        music.start_quiz()
+        music.start_quiz() 
     else:
         print("Invalid choice. Please try again.")
 
